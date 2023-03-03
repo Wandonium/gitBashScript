8d694c22d1bc025c1da9d9c5a08c7c6315cf2ea4
Wandonium <hillarywando@gmail.com>
2023-03-03 10:23:07 +0300

    Added cron_echo file to receive crontab echos from the script.sh bash script.

--- script.sh
+++ script.sh
@@ -8,11 +8,11 @@ message="Commit for $date"
 git add .
 git commit -m"${message}"
 status="$(git status --branch --porcelain)"
-echo $status >> ~/gitBashScript/cron_test.txt
+echo $status >> ~/gitBashScript/cron_echo.txt
 if [ "$status" == "## master...origin/master" ]; then
-  echo "IT IS CLEAN" >> ~/gitBashScript/cron_test.txt
+  echo "IT IS CLEAN" >> ~/gitBashScript/cron_echo.txt
 else
-  echo "There is stuff to push" >> ~/gitBashScript/cron_test.txt
+  echo "There is stuff to push" >> ~/gitBashScript/cron_echo.txt
   git push -u origin master
 fi
 
