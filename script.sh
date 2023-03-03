798df4e71bef45d4ecb8701ed703153c627b560c
Wandonium <hillarywando@gmail.com>
2023-03-03 10:17:14 +0300

    Added code to script.sh to keep generating stuff to commit every 10 minutes using crontab.

--- script.sh
+++ script.sh
@@ -8,10 +8,12 @@ message="Commit for $date"
 git add .
 git commit -m"${message}"
 status="$(git status --branch --porcelain)"
-echo $status
+echo $status >> ~/gitBashScript/cron_test.txt
 if [ "$status" == "## master...origin/master" ]; then
-  echo "IT IS CLEAN"
+  echo "IT IS CLEAN" >> ~/gitBashScript/cron_test.txt
 else
-  echo "There is stuff to push"
+  echo "There is stuff to push" >> ~/gitBashScript/cron_test.txt
   git push -u origin master
 fi
+
+echo "Today is: $date" >> ~/gitBashScript/cron_test.txt
