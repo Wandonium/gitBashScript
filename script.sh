aa66b6c5afb21447e611cd7810304253534ab818
Wandonium <hillarywando@gmail.com>
2023-03-03 10:03:47 +0300

    Added git commands to script.sh.

--- script.sh
+++ script.sh
@@ -3,4 +3,17 @@
 # Date in format Day-Month-Year
 date=$(date +%d-%m-%Y)
 
+# Commit message
+message="Commit for $date"
+git add .
+git commit -m"${message}"
+status="$(git status --branch --porcelain)"
+echo $status
+if [ "$status" == "## master...origin/master" ]; then
+  echo "IT IS CLEAN"
+else
+  echo "There is stuff to push"
+  git push -u origin master
+fi
+
 echo "Today is : $date" >> ~/gitBashScript/cron_test.txt
