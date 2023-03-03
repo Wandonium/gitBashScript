723fdc2c5e82d328f138fcc8be624b9e98d1fa2c
Wandonium <hillarywando@gmail.com>
2023-03-03 10:46:51 +0300

    Commit for 2023-03-03 10:46:51

--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 # Date in format Day-Month-Year
-date=$(date +%d-%m-%Y)
+date=$(date +"%Y-%m-%d %T")
 
 # Commit message
 message="Commit for $date"
 git add .
 git commit -m"${message}"
 status="$(git status --branch --porcelain)"
-echo $status >> ~/gitBashScript/cron_echo.txt
+echo $status >> ~/cron_echo.txt
 if [ "$status" == "## master...origin/master" ]; then
-  echo "IT IS CLEAN" >> ~/gitBashScript/cron_echo.txt
+  echo "IT IS CLEAN" >> ~/cron_echo.txt
 else
-  echo "There is stuff to push" >> ~/gitBashScript/cron_echo.txt
+  echo "There is stuff to push" >> ~/cron_echo.txt
   git push -u origin master
 fi
 
